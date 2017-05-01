38772131ebdc3d42ae8b756b86a2028e86939fe8
DESKTOP-EC9BMGP\nuch_ <oranuch901@gmail.com>
2017-05-01 21:39:26 +0700

    Calculate cmb and return JSON

---
+++ app/Http/Controllers/Api/CalculatorController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CalculatorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //width, length, hight in centimeter(cm.)
+        $w = $request->width;
+        $l = $request->length;
+        $h = $request->height;
+        $cbm = $w*$l*$h/1000000;
+        $weight = $cbm*150;
+        $q = $request->priceType;
+        $total = ceil($q*$cbm);
+
+        return [
+          'success'=>true,
+          'data'=> [
+            'weight' => $weight,
+            'total' => $total
+          ]
+        ];
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
